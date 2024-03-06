7f20302a3ad3fa3b5923d59d72a78793ae779541
Ronaldinho-OrtegaRuiz <rortegar01@libertadores.edu.co>
2024-03-06 00:12:15 -0500

    Añadiendo el componente repository con CrudRepository de Spring Data JPA

---
+++ src/main/java/Repositories/PersonaRepository.java
@@ -0,0 +1,8 @@
+package Repositories;
+
+import entities.Person;
+import org.springframework.data.repository.CrudRepository;
+
+public interface PersonaRepository extends CrudRepository<Person, Long>{
+
+}
--- src/main/java/com/andres/curso/springboot/jpa/springbootjpa/SpringbootJpaApplication.java
+++ src/main/java/com/andres/curso/springboot/jpa/springbootjpa/SpringbootJpaApplication.java
@@ -1,11 +1,19 @@
 package com.andres.curso.springboot.jpa.springbootjpa;
 
+import Repositories.PersonaRepository;
+import entities.Person;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.List;
+
 @SpringBootApplication
 public class SpringbootJpaApplication implements CommandLineRunner {
+
+	@Autowired
+	private PersonaRepository repository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootJpaApplication.class, args);
@@ -14,6 +22,7 @@ public class SpringbootJpaApplication implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 
-
+		List<Person> persons = (List<Person>) repository.findAll();
+		persons.stream().forEach(person -> System.out.println(person.getName()));
 	}
 }
