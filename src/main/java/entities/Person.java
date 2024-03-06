ad549c9d15224450298ee383b044e02f31ffe2fc
Ronaldinho-OrtegaRuiz <rortegar01@libertadores.edu.co>
2024-03-05 23:48:18 -0500

    Añadiendo la clase Entity

---
+++ src/main/java/entities/Person.java
@@ -0,0 +1,68 @@
+package entities;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "persons")
+public class Person {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String lastName;
+    @Column(name = "programming_language")
+    private String programmingLanguage;
+
+    public Person() {
+    }
+
+    public Person(Long id, String name, String lastName, String programmingLanguage) {
+        this.id = id;
+        this.name = name;
+        this.lastName = lastName;
+        this.programmingLanguage = programmingLanguage;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getProgrammingLanguage() {
+        return programmingLanguage;
+    }
+
+    public void setProgrammingLanguage(String programmingLanguage) {
+        this.programmingLanguage = programmingLanguage;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", programmingLanguage='" + programmingLanguage + '\'' +
+                '}';
+    }
+}
